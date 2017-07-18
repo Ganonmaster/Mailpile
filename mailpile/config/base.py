3987b284fc2b6e5eb9047b76d0bc6defbb142502
Hidde Jansen <hjansen@transip.nl>
2017-07-18 22:22:15 +0200

    Modernize print formatting on config package.

--- mailpile/config/base.py
+++ mailpile/config/base.py
@@ -711,6 +711,6 @@ if __name__ == "__main__":
     import doctest
     import sys
     result = doctest.testmod(optionflags=doctest.ELLIPSIS)
-    print '%s' % (result, )
+    print('{}'.format(result))
     if result.failed:
         sys.exit(1)
--- mailpile/config/validators.py
+++ mailpile/config/validators.py
@@ -358,6 +358,6 @@ if __name__ == "__main__":
     import doctest
     import sys
     result = doctest.testmod(optionflags=doctest.ELLIPSIS)
-    print '%s' % (result, )
+    print('{}'.format(result))
     if result.failed:
         sys.exit(1)
